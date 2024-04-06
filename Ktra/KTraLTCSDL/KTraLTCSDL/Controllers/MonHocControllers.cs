f34c62931b5e459bcba955ec42d7a3907d07fb21
NguyenDuong0012 <151751356+NguyenDuong0012@users.noreply.github.com>
2024-04-06 15:01:16 +0700

    Add files via upload

---
+++ Ktra/KTraLTCSDL/KTraLTCSDL/Controllers/MonHocControllers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KtraLTCSDL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonHocController : ControllerBase
+    {
+        
+        private readonly List<MonHoc> danhSachMonHoc = new List<MonHoc>
+        {
+            new MonHoc { MaMon = 1, TenMon = "Mon 1", SoTinChi = 3 },
+            new MonHoc { MaMon = 2, TenMon = "Mon 2", SoTinChi = 4 },
+          
+        };
+
+        [HttpGet("{maNganh}")]
+        public IEnumerable<MonHoc> GetMonHoc(int maNganh)
+        {
+            
+            return danhSachMonHoc.Where(m => m.MaNganh == maNganh);
+        }
+    }
+}
---
+++ Ktra/KTraLTCSDL/KTraLTCSDL/Controllers/SinhVienController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace KtraLTCSDL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SinhVienController : ControllerBase
+    {
+        private readonly List<SinhVienNganhHoc> danhSachSinhVienNganhHoc = new List<SinhVienNganhHoc>();
+
+        [HttpPut("{maSinhVien}")]
+        public IActionResult ThayDoiMaNganhCuaSinhVien(int maSinhVien, int maNganhMoi)
+        {
+          
+            var sinhVien = danhSachSinhVienNganhHoc.FirstOrDefault(sv => sv.MaSinhVien == maSinhVien);
+            if (sinhVien == null)
+            {
+                return NotFound("Không tìm thấy sinh viên.");
+            }
+
+          
+            if (danhSachSinhVienNganhHoc.Any(sv => sv.MaSinhVien != maSinhVien && sv.MaNganh == maNganhMoi))
+            {
+                return BadRequest("Mã ngành mới đã tồn tại cho sinh viên khác.");
+            }
+
+           
+            sinhVien.MaNganh = maNganhMoi;
+
+            return Ok("Thay đổi mã ngành của sinh viên thành công.");
+        }
+    }
+}
